0dc88a4784604deebbb735d5fb77edc19b301ec7
promisetuhaise <promisetuhaise@gmail.com>
2024-10-21 00:04:27 -0700

    test 4 and 5 successful

--- test/test.js
+++ test/test.js
@@ -22,3 +22,17 @@ it('should return 4 when multiplying 2 by 2', function () {
  it('should return 9 when multiplying 3 by 3', function () {
   expect(multiply(3, 3)).to.equal(9);
 });
+
+
+//test 4
+it('should return 16 when multiplying 4 by 4', function () {
+  expect(multiply(4, 4)).to.equal(16);
+});
+
+
+
+//test 5
+it('should return 1035 when multiplying 23 by 45', function () {
+  expect(multiply(23, 45)).to.equal(23 * 45);
+});
+
